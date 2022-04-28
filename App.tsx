be8ba244f9464e7a42c402b0636ea0a85fc31049
Marcelo Andrade <marcelo.andrade@itsector.pt>
2022-04-28 17:16:33 +0100

    [ITN] - Added Web View Component

--- App.tsx
+++ App.tsx
@@ -1,115 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Linking, SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import WebView, {WebViewMessageEvent} from 'react-native-webview';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {} from 'react-native';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+  };
+
+  // const webViewScript = `window = { ...window, techOClock: { isMobileApp: true}`;
+
+  const webViewScript = `
+    // set isMobileApp Here
+    window.isNativeApp = true;
+    true; 
+  `;
+
+  const handleCommunicationBridgeEvents = (
+    event: WebViewMessageEvent,
+  ): void => {
+    console.log('Web View > handleCommunicationBridgeEvents > event >', event);
+
+    const {type} = JSON.parse(event.nativeEvent.data);
+    if (type) {
+      switch (type) {
+        case 'OPEN_CONTACTS':
+          console.log(
+            'Web View > handleCommunicationBridgeEvents > Open Contacts',
+          );
+          Linking.openURL('content://com.android.contacts/contacts');
+          break;
+        default:
+          return;
+      }
+    }
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <StatusBar barStyle={'dark-content'} />
+      <View style={backgroundStyle}>
+        <WebView
+          source={{
+            uri: 'https://google.pt',
+          }}
+          javaScriptEnabled={true}
+          automaticallyAdjustContentInsets={false}
+          injectedJavaScriptBeforeContentLoaded={webViewScript}
+          onMessage={handleCommunicationBridgeEvents}
+        />
+      </View>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
